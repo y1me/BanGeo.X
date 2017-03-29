54ecf18ae47f2338639ab43e67e348b8788b0e6a
Pierre <pierre.yameogo@mobirider.com>
2017-03-29 18:24:01 +0200

    Add EEPROM read/write function

--- user.c
+++ user.c
@@ -21,6 +21,91 @@ extern volatile char RX_BUFF[32];
 extern volatile char TX_BUFF[32];
 extern volatile unsigned char error, *pRX_W, *pTX_stop, *pTX_W;
 
+/**
+  @Summary
+    Writes a data byte to Data EEPROM
+
+  @Description
+    This routine writes a data byte to given Data EEPROM location
+
+  @Preconditions
+    None
+
+  @Param
+    bAdd  - Data EEPROM location to which data to be written
+    bData - Data to be written to Data EEPROM location
+
+  @Returns
+    None
+
+  @Example
+    <code>
+    uint16_t dataeeAddr = 0xF010;
+    uint8_t dataeeData = 0x55;
+
+    DATAEE_WriteByte(dataeeAddr, dataeeData);
+    </code>
+*/
+
+void DATAEE_WriteByte(uint16_t bAdd, uint8_t bData)
+{
+    uint8_t GIEBitValue = INTCONbits.GIE;
+
+    NVMADRH = ((bAdd >> 8) & 0xFF);
+    NVMADRL = (bAdd & 0xFF);
+    NVMDATL = bData;    
+    NVMCON1bits.NVMREGS = 1;
+    NVMCON1bits.WREN = 1;
+    INTCONbits.GIE = 0;     // Disable interrupts
+    NVMCON2 = 0x55;
+    NVMCON2 = 0xAA;
+    NVMCON1bits.WR = 1;
+    // Wait for write to complete
+    while (NVMCON1bits.WR)
+    {
+    }
+
+    NVMCON1bits.WREN = 0;
+    INTCONbits.GIE = GIEBitValue;   // restore interrupt enable
+}
+
+/**
+  @Summary
+    Reads a data byte from Data EEPROM
+
+  @Description
+    This routine reads a data byte from given Data EEPROM location
+
+  @Preconditions
+    None
+
+  @Param
+    bAdd  - Data EEPROM location from which data has to be read
+
+  @Returns
+    Data byte read from given Data EEPROM location
+
+  @Example
+    <code>
+    uint16_t dataeeAddr = 0xF010;
+    uint8_t readData;
+
+    readData = DATAEE_ReadByte(dataeeAddr);
+    </code>
+*/
+
+uint8_t DATAEE_ReadByte(uint16_t bAdd)
+{
+    NVMADRH = ((bAdd >> 8) & 0xFF);
+    NVMADRL = (bAdd & 0xFF);
+    NVMCON1bits.NVMREGS = 1;    
+    NVMCON1bits.RD = 1;
+    NOP();  // NOPs may be required for latency at high frequencies
+    NOP();
+
+    return (NVMDATL);
+}
+
 void InitI2cChip(void)
 {
     char data[2];
